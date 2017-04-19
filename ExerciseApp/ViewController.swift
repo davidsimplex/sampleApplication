8d99117e7f51732bb8a6be57d16d60303e0de802
David Muñoz <david.munoz@simplex.software>
2017-04-19 18:00:02 -0300

    Initial Commit

---
+++ ExerciseApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExerciseApp
+//
+//  Created by Sierra2 on 19/4/17.
+//  Copyright © 2017 Sierra2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

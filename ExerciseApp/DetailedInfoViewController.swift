759ec517b951e25cba77520e4f7bc5e6eb8ba3b4
David Muñoz <david.munoz@simplex.software>
2017-04-20 12:33:14 -0300

    first commit

---
+++ ExerciseApp/DetailedInfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailedInfoViewController.swift
+//  ExerciseApp
+//
+//  Created by Sierra2 on 20/4/17.
+//  Copyright © 2017 Sierra2. All rights reserved.
+//
+
+import UIKit
+
+class DetailedInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ExerciseApp/WebhoseService.swift
@@ -0,0 +1,13 @@
+//
+//  WebhoseService.swift
+//  ExerciseApp
+//
+//  Created by Sierra2 on 19/4/17.
+//  Copyright © 2017 Sierra2. All rights reserved.
+//
+
+import UIKit
+
+class WebhoseService: NSObject {
+
+}
